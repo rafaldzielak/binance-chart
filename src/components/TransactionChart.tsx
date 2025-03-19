b6becdbe46fca8cf15fa8d45588c9618f8e5dbd3
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2025-03-19 12:08:15 +0100

    Refetch data every 20 secs and show loading

--- src/components/TransactionChart.tsx
+++ src/components/TransactionChart.tsx
@@ -1,25 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 
+import dayjs from "dayjs";
 import ReactECharts from "echarts-for-react";
 import { FC } from "react";
 import { bincanceDataToObject, DataFromBinance } from "../utils/binanceDataConverter";
-import dayjs from "dayjs";
 
 const getBinanceData = async () => {
-  const res = await fetch("http://127.0.0.1:8080/https://binance.com/api/v3/klines?symbol=ETHBTC&interval=1h");
+  const res = await fetch("http://127.0.0.1:8080/https://binance.com/api/v3/klines?symbol=ETHBTC&interval=1s");
   const data: DataFromBinance[] = await res.json();
   const convertedData = bincanceDataToObject(data);
   return convertedData;
 };
 
 const TransactionChart: FC = () => {
-  const { data, isLoading, error } = useQuery({ queryKey: ["binanceData"], queryFn: getBinanceData });
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["binanceData"],
+    queryFn: getBinanceData,
+    refetchInterval: 20000,
+  });
 
-  console.log({ data, isLoading, error });
+  console.log({ error });
 
   return (
-    <>
+    <div>
       <ReactECharts
+        showLoading={isLoading}
         style={{ width: "500px", height: "500px" }}
         option={{
           title: {
@@ -32,7 +37,7 @@ const TransactionChart: FC = () => {
           grid: { top: 8, right: 8, bottom: 24, left: 36 },
           xAxis: {
             type: "category",
-            data: data?.map((unit) => dayjs(unit.klineOpenTime).format("DD-MM HH-MM")),
+            data: data?.map((unit) => dayjs(unit.klineOpenTime).format("DD-MM HH:MM:ss")),
           },
           yAxis: {
             type: "value",
@@ -49,7 +54,7 @@ const TransactionChart: FC = () => {
           },
         }}
       />
-    </>
+    </div>
   );
 };
 
