714250381d27495bf2b5d9da88b1643cdf1ebf57
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2025-03-19 11:40:47 +0100

    Handle cors erros with cors-anywhere & fetch the data

--- src/components/TransactionChart.tsx
+++ src/components/TransactionChart.tsx
@@ -1,4 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+
+const getBinanceData = async () => {
+  const res = await fetch("http://127.0.0.1:8080/https://binance.com/api/v3/klines?symbol=ETHBTC&interval=1h");
+  const data = await res.json();
+  return data;
+};
+
 const TransactionChart = () => {
+  const { data, isLoading, error } = useQuery({ queryKey: ["binanceData"], queryFn: getBinanceData });
+
+  console.log({ data, isLoading, error });
   return <div>TransactionChart</div>;
 };
 
